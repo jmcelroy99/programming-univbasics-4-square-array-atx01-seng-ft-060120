6075d0c6feb15a6a785e7f660f478db581d5eb55
energetic-commit-5967 <jmcelroy@plexustech.net>
2020-04-17 16:36:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
   # your code here
+  count = 0
+  result = []
+  while count < array.length do
+    squared = array[count] ** 2
+    count += 1
+    result.push(squared)
+  end
 end
